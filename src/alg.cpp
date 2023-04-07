8434100847892870d99a25dcd41f8c6c6f876023
MishaLybin <116936366+MishaLybin@users.noreply.github.com>
2023-04-07 18:31:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,68 +1,56 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int* arr, int len, int value)
-{
-	int a = 0;
-	for (int i = 0; i < len-1; i++)
-	{
-		for (int j = i + 1; j < len; j++)
-		{
-			if (arr[i] + arr[j] == value)
-				a++;
-		}
-	}
-
-	return a;
+int countPairs1(int *arr, int len, int value) {
+  int a = 0;
+  for (int i = 0; i < len - 1; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
+        a++;
+      }
+    }
+  }
+  return sc;
 }
-int countPairs2(int* arr, int len, int value)
-{
-	int a = 0;
-	for (int i = 0; i < len - 1; i++)
-	{
-		for (int j = len - 1; j > i; j--)
-		{
-			if (arr[i] + arr[j] == value)
-			{
-				a++;
-
-			}
-		}
-	}
-	return a;
+int countPairs2(int *arr, int len, int value) {
+  int a = 0;
+  for (int i = 0; i < len - 1; i++) {
+    for (int j = len - 1; j > i; j--) {
+      if (arr[i] + arr[j] == value) {
+        a++;
+      }
+    }
+  }
+  return a;
 }
-int countPairs3(int* arr, int len, int value)
-{int a = 0;
-	for (int i = 0; i < len - 1; i++)
-	{
-		int left = i, right = len;
-		
-		while (left < right-1)
-		{
-			int mid = (left + right) / 2;
-			if (arr[i] + arr[mid] == value)
-			{
-				a++;
-				int j = mid+1;
-				while (arr[i] + arr[j] == value && j <right)
-				{
-					a++;
-					j++;
-				}
-				j = mid - 1;
-				while (arr[i] + arr[j] == value && j>left)
-				{
-					a++;
-					j--;
-				}
-				break;
-			}
-			if (arr[i] + arr[mid] > value)
-				right = mid;
-			else
-				left = mid;
-		}
-	
-	}
-	
-	
-	return a;
+int countPairs3(int *arr, int len, int value) {
+  int q;
+  int a;
+  int n = 0;
+  for (int i = 0; i < len; i++) {
+    int left = i;
+    int right = len;
+    while (left < right-1) {
+      q = (left + right) / 2;
+      if (arr[i] + arr[q] == value) {
+        n++;
+        a = 1;
+        while (arr[q] == arr[q + a] && (q + a) < right) {
+          n++;
+          a++;
+        }
+	  a = 1;
+        while (arr[q] == arr[q - a] && (q - a) > left) {
+          n++;
+          a++;
+        }
+        break;
+      }
+      if (arr[i] + arr[q] > value) {
+        right = q;
+      }
+      if (arr[i] + arr[q] < value) {
+        left = q;
+      }
+    }
+  }
+  return n;
 }
